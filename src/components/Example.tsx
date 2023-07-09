8b36f129aea928dd72158f144b386ef72383d87f
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-07-09 10:06:05 +0200

    fix canvas size problem

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,13 +1,12 @@
-import { useLoader } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { Canvas } from "./Canvas";
 
 export const Example = () => {
   const gltf = useLoader(GLTFLoader, "/assets/poly2.glb");
 
   return (
-    <Canvas>
+    <Canvas resize={{ scroll: false, offsetSize: true }}>
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
